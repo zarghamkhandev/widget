e694267283e2c78c41691480a95f9c166bfb1d09
zarghamkhandev <zarghamkhan980@gmail.com>
2020-12-02 13:53:15 +0100

    list items and filter logic added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IoAddOutline } from 'react-icons/io5';
+import { uniqueNamesGenerator, names } from 'unique-names-generator';
+import { initialNames } from '../utils/initialArray';
 export default function Home() {
+  const [namesList, setNamesList] = useState<string[]>(initialNames);
+  const [searchInput, setSearchInput] = useState<string>('');
+  const filteredList = namesList.filter((name) =>
+    name.toLowerCase().includes(searchInput.toLowerCase())
+  );
+  const addNewItem = () => {
+    const randomName: string = uniqueNamesGenerator({
+      dictionaries: [names, names],
+      separator: ' ',
+      style: 'capital',
+    });
+    setNamesList((prevState) => {
+      return [...prevState, randomName];
+    });
+  };
+
   return (
     <div className="min-h-screen w-full bg-gray-50 flex items-center justify-center">
       <div className="w-72 rounded-md border border-gray-500 shadow-xl flex flex-col overflow-hidden ">
@@ -9,17 +27,28 @@ export default function Home() {
             <input
               className="rounded pl-2 py-1 border border-gray-500"
               placeholder="Search"
+              value={searchInput}
+              onChange={async (event) => {
+                setSearchInput(event.target.value);
+              }}
             />
           </div>
           <div className="w-1/5 flex border-l border-gray-500 items-center justify-center ">
-            <IoAddOutline className="w-8 h-8 text-white hover:text-gray-200 cursor-pointer" />
+            <IoAddOutline
+              className="w-8 h-8 text-white hover:text-gray-200 cursor-pointer"
+              onClick={addNewItem}
+            />
           </div>
         </div>
-        <div className="h-96 bg-gray-200">
+        <div className="h-96 bg-gray-200 overflow-y-auto">
           <ul className="w-full">
-            <li className="pt-1 mt-1 mx-2 pl-2 border-b border-gray-400 ">
-              hi
-            </li>
+            {filteredList.map((name, index) => (
+              <li
+                className="pt-1 mt-1 mx-2 pl-2 border-b border-gray-400 "
+                key={index}>
+                {name}
+              </li>
+            ))}
           </ul>
         </div>
       </div>
---
+++ src/utils/initialArray.ts
@@ -0,0 +1,8 @@
+export const initialNames = [
+  'Jenn Cheryl',
+  'Dru Ki',
+  'Sabra Morgen',
+  'Lynnette Leta',
+  'Allx Rycca',
+  'Terrye Trudi',
+];
